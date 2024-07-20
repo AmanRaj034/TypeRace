68273f17b84c12732b926fd9bbbfade2b4b535d2
AMAN RAJ <duohim877@mirai.re>
2024-07-21 03:38:12 +0530

    connected server and fronted

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,23 @@ import dotenv from "dotenv";
 import colors from "colors";
 import wordsRoutes from "./routes/wordsRoutes.js";
 import cors from "cors";
+import path from "path";
 import { server, app } from "./socketHandler/UsersConnection.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/fronted/dist")));
+
 app.use(express.json());
 app.use(cors());
 
 
-app.get("/", (req, res) => {
-    res.send(`Hi From Server`);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "fronted", "dist", "index.html"));
 })
 
 app.use("/api/words", wordsRoutes);
